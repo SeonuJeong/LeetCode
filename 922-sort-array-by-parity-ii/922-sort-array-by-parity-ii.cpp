17548fd71c3dfcabef2db2886667c047d6f85031
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-01-16 18:12:12 +0900

    Time: 16 ms (91.79%), Space: 21.5 MB (26.12%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -4,24 +4,29 @@ public:
         int i=0;
         int odd=0, even=0;
         
-        vector<int> result(nums.size(),0);
-        sort(nums.begin(), nums.end());
-
-        while(i<result.size()){
-            
-            if(i%2==0){
-                while(nums[even]%2!=0){
-                    even++;
-                }
-                result[i++] = nums[even++];
-            }
-            else{
-                while(nums[odd]%2!=1){
+        while(i<nums.size()){
+            if(i%2==1 && nums[i]%2==0){
+                if(odd<i) odd = i;
+                
+                while(nums[odd]%2==0){
                     odd++;
                 }
-                result[i++] = nums[odd++];
+                
+                swap(nums[i],nums[odd]);
             }
+            else if(i%2==0 && nums[i]%2==1){
+                if(even<i) even = i;
+                
+                while(nums[even]%2==1){
+                    even++;
+                }
+                
+                swap(nums[i],nums[even]);
+            }
+            
+            i++;
         }
-        return result;
+        
+        return nums;
     }
 };
