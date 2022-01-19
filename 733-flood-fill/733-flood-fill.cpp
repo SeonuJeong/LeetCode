e32e9797e039b718078d47219718e19744cf9fcf
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-01-19 14:44:12 +0900

    Time: 26 ms (5.05%), Space: 14.1 MB (51.80%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -4,22 +4,22 @@ public:
     int delta[4][2] = {{1,0},{-1,0},{0,-1},{0,1}};//up,down,left,right
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         
-        floodFill(image,sr,sc,newColor,image[sr][sc]);
+        dfs(image,sr,sc,newColor,image[sr][sc]);
         
         return image;
     }
     
-    void floodFill(vector<vector<int>>& image,int row, int col, int newColor, int value){
+    void dfs(vector<vector<int>>& image,int row, int col, int newColor, int value){
         if(row<0||row>=image.size()||col<0||col>=image[0].size()||image[row][col]!=value
           ||image[row][col]==newColor)
             return;
         
         image[row][col] = newColor;
         
-        floodFill(image,row+1,col+0,newColor,value);
-        floodFill(image,row-1,col+0,newColor,value);
-        floodFill(image,row+0,col+1,newColor,value);
-        floodFill(image,row+0,col-1,newColor,value);
+        dfs(image,row+1,col+0,newColor,value);
+        dfs(image,row-1,col+0,newColor,value);
+        dfs(image,row+0,col+1,newColor,value);
+        dfs(image,row+0,col-1,newColor,value);
         
     }
     
