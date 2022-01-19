6903266a0428559f1111f6dab90579c4f046de35
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-01-19 14:42:31 +0900

    Time: 11 ms (53.76%), Space: 14 MB (51.80%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -3,14 +3,15 @@ public:
    
     int delta[4][2] = {{1,0},{-1,0},{0,-1},{0,1}};//up,down,left,right
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(image[sr][sc]!=newColor)
-            floodFill(image,sr,sc,newColor,image[sr][sc]);
+        
+        floodFill(image,sr,sc,newColor,image[sr][sc]);
         
         return image;
     }
     
     void floodFill(vector<vector<int>>& image,int row, int col, int newColor, int value){
-        if(row<0||row>=image.size()||col<0||col>=image[0].size()||image[row][col]!=value)
+        if(row<0||row>=image.size()||col<0||col>=image[0].size()||image[row][col]!=value
+          ||image[row][col]==newColor)
             return;
         
         image[row][col] = newColor;
