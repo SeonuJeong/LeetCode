9a14d4f763373b258338e981c9c858ea0bca36eb
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-01-25 18:32:48 +0900

    Time: 23 ms (62.95%), Space: 21.2 MB (23.14%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+       
+        vector<int> dp(flowerbed.size(),0);
+        
+        if(flowerbed[0]==0&&flowerbed.size()==1)
+            dp[0] = 1;
+        else if(flowerbed[0]==0&&flowerbed[1]==0){
+            dp[0] = 1;
+            flowerbed[0] = 1;
+        }
+        
+        
+        for(int i=1; i<flowerbed.size();i++){
+            
+            if(i+1 ==flowerbed.size()){
+                if(flowerbed[i-1]==0 && flowerbed[i]==0) dp[i] = dp[i-1]+1;
+                else dp[i] = dp[i-1];
+                continue;
+            }
+            
+            
+            if(flowerbed[i+1]==0 && flowerbed[i-1]==0 && flowerbed[i]==0){
+                dp[i] = dp[i-1]+1;
+                flowerbed[i] = 1;
+            }
+            else
+                dp[i] = dp[i-1];
+        }
+        
+        if(dp[flowerbed.size()-1]-n>=0)
+            return true;
+        else 
+            return false;
+        
+    }
+    
+    
+};
