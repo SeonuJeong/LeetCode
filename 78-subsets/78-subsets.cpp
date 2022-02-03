7848bb28e1c875dcc4547a201ab78b6c1af1da66
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-02-04 00:46:58 +0900

    Time: 6 ms (26.53%), Space: 7.4 MB (46.00%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    vector<int> nums;
-    vector<vector<int>> result;
     vector<vector<int>> subsets(vector<int>& nums) {
-        this->nums = nums;
-        dfs(0);
-        result.push_back(vector<int>(0));
-        return result;
+        vector<vector<int>> output(1,vector<int>(0));
+        
+        for(int num : nums){
+            vector<vector<int>> newSubsets;
+            
+            for(vector<int> curr : output){
+                curr.push_back(num);
+                newSubsets.push_back(curr);
+            }
+            
+            for(vector<int> curr : newSubsets){
+                output.push_back(curr);    
+            }
+     
+        }
+        
+        return output;
     }
     
-    void dfs(int i){
-        
-        if(i==nums.size()-1){
-            result.push_back(vector<int>(1,nums[i]));
-            return;
-        }
-        
-        dfs(i+1);
-        int size = result.size();        
-        for(int j=0; j<size; j++){
-            vector<int> tmp = result[j];
-            tmp.push_back(nums[i]);
-            result.push_back(tmp);
-        }
-        
-        result.push_back(vector<int>(1,nums[i]));
-        
-        return;
-    }
 };
