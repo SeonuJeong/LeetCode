940a3979198878de200891ad97a3e0a35483491b
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-02-04 09:27:41 +0900

    Time: 4 ms (57.72%), Space: 7.2 MB (52.34%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
+    vector<vector<int>> output;
+    vector<int> nums;
+    int k;
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> output(1,vector<int>(0));
+        this->nums = nums;
         
-        for(int num : nums){
-            vector<vector<int>> newSubsets;
-            
-            for(vector<int> curr : output){
-                curr.push_back(num);
-                newSubsets.push_back(curr);
-            }
-            
-            for(vector<int> curr : newSubsets){
-                output.push_back(curr);    
-            }
-     
+        for(k=0; k<=nums.size(); k++){
+            vector<int> tmp(0);
+            backtrack(0,tmp);
+        }
+        return output;
+    }
+    
+    
+    void backtrack(int cur, vector<int>& subset){
+        
+        if(subset.size()==k){
+            output.push_back(subset);
+            return;
         }
         
-        return output;
+        for(int i=cur; i<nums.size(); i++){
+            subset.push_back(nums[i]);
+            backtrack(i+1, subset);
+            subset.pop_back();
+        }
+        
+        return;
     }
     
 };
