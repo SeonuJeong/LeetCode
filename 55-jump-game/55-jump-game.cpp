c5f861a6632b8aecac4529175d33426d1bb7fdc1
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-02-06 16:13:50 +0900

    Time: 1168 ms (5.11%), Space: 53 MB (5.54%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     //03:44
+    //03:58
+    vector<int> cache;
+    int N;
+    vector<int> nums;
     bool canJump(vector<int>& nums) {
-        int last=nums.size()-1;
-        int index=last-1;
+        cache.assign(nums.size(),-1);
+        N = nums.size();
+        this->nums = nums;
         
-        if(last==0)return true;
+        return dp(0);
+    }
+    
+    bool dp(int index){
         
-        while(index>=0){
-            
-            if(nums[index]>= last-index){ // can reach last index
-                last = index;
-                index = index-1;
-            }
-            else{ // can't reach last index
-                index = index-1;
-            }
-            
+        if(index>=N-1){
+            return true;
         }
         
-        if(last==0) return true;
-        else return false;
+        if(cache[index]!=-1)
+            return cache[index];
+        
+        bool result=false;
+        for(int i=nums[index]; i>0; i--){
+            if(dp(index+i)){
+                result = true;
+                break;
+            }
+        }
+        
+        if(result==true) cache[index] = 1;
+        else cache[index] = 0;
+        
+        return result;
         
     }
 };
