1cfd51d3eeecb753d4c769fd55f2c244e6735adb
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-02-08 18:16:03 +0900

    Time: 712 ms (14.61%), Space: 397.6 MB (5.00%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    // 05:01
+    
+    vector<vector<int>> cache;
+    string s;
+    string output="";
+    
+    string longestPalindrome(string s) {
+        cache.assign(s.length(), vector<int>(s.length(),0));
+        this->s = s;
+        
+        for(int i=0; i<s.length(); i++){
+            dp(0,i);
+        }
+        
+        return output;
+    }
+    
+    int dp(int left, int right){
+        
+        
+        if(left > right){
+            return 1;
+        }
+        
+        if(left==right){
+            if(output.length() < 1)
+                output = s.substr(left,1);
+            
+            return 1;
+        }
+        
+        if(cache[left][right]!=0){
+            return cache[left][right];
+        }
+        
+        int result=-1;
+        for(int i=left; i<right; i++){
+            if(s[i]==s[right]){
+                if(dp2(i+1, right-1)>0){
+                    result= right-i+1;
+                    if(output.length() < s.substr(i,right-i+1).length())
+                        output = s.substr(i,right-i+1);
+                    break;
+                }
+            }
+        }
+        
+        cache[left][right] = result;
+        return result;
+    }
+    
+    
+    int dp2(int left,int right){
+        
+        if(left==right) return 1;
+        if(left>right) return 1;
+        
+        if(s[left]==s[right]){
+            return dp2(left+1,right-1);
+        }
+        else{
+            return -1;
+        }
+    }
+};
