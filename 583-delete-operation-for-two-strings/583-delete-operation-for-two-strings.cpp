516be98d1ed2b12682be0b6a7d3bd6ff9b2d2686
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-02-11 16:58:19 +0900

    Time: 30 ms (43.62%), Space: 12.3 MB (15.09%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -2,38 +2,33 @@ class Solution {
 public:
     //03:56
     //04:14
-    vector<vector<int>> cache;
-    string word1;
-    string word2;
+    vector<vector<int>> dp;
+
     int minDistance(string word1, string word2) {
-        cache.assign(word1.length(),vector<int>(word2.length(),-1));
-        // row = word1 , col = word2
+        dp.assign(word1.length()+1,vector<int>(word2.length()+1,0));
+        // row = word1 , col = word
         
-        this->word1 = word1;
-        this->word2 = word2;
+        for(int i=0; i<word1.length(); i++){
+            dp[i][word2.length()] = word1.length()-i;
+        }
         
-        return dp(word1.length()-1, word2.length()-1);
+        for(int i=0; i<word2.length(); i++){
+            dp[word1.length()][i] = word2.length()-i;
+        }
         
+        for(int y=word1.length()-1; y>=0; y--){
+            for(int x=word2.length()-1; x>=0; x--){
+                if(word1[y]==word2[x]){
+                    dp[y][x] = dp[y+1][x+1];
+                }
+                else{
+                    dp[y][x] = min(dp[y+1][x],dp[y][x+1])+1;
+                }
+            }
+        }
+        
+        return dp[0][0];
     }
     
-    int dp(int y, int x){
-        
-        if(y==-1 || x==-1){
-            return y==-1?x+1:y+1;
-        }
-        
-        if(cache[y][x]!=-1){
-            return cache[y][x];
-        }
-        
-        if(word1[y] == word2[x]){
-             cache[y][x] = dp(y-1,x-1);
-        }
-        else{
-             cache[y][x] = min(dp(y-1,x),dp(y,x-1))+1;
-        }
-        
-        
-        return cache[y][x];
-    }
+    
 };
