5d7754cdf1ad683ea9f8736290118aaa1930e18b
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-02-15 19:34:20 +0900

    Time: 8 ms (49.60%), Space: 8.3 MB (46.36%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> cache;
+    
+    vector<int> countBits(int n) {
+        cache.assign(n+1,-1);
+        
+        vector<int> result(n+1);
+        
+        for(int i=0; i<n+1; i++){
+            result[i] = dp(i);    
+        }
+        
+        return result;
+    }
+    
+    int dp(int n){
+        
+        if(n==0)
+            return 0;
+        
+        if(cache[n]!=-1){
+            return cache[n];
+        }
+        
+        int cur = n & 1;
+        
+        cache[n] = dp(n>>1)+cur;
+        
+        return cache[n];
+    }
+    
+};
