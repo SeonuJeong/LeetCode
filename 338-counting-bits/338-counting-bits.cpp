547eca347abf121a2c6c4fc1e4d074491b0ea7e2
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-02-15 19:34:54 +0900

    Time: 3 ms (94.86%), Space: 8.3 MB (46.36%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -7,7 +7,7 @@ public:
         
         vector<int> result(n+1);
         
-        for(int i=0; i<n+1; i++){
+        for(int i=n; i>=0; i--){
             result[i] = dp(i);    
         }
         
