6ae473ceee1ed0f8bf20857f877fa7bc1471a83c
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-02-17 08:53:45 +0900

    Time: 24 ms (83.31%), Space: 10.6 MB (50.04%) - LeetHub

---
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    //08:30
+    int countBinarySubstrings(string s) {
+        
+        int before=0;
+        int cur=1;
+        
+        if(s.length()==1) return 0;
+        
+        int count=0;
+        for(int i=1; i<s.length(); i++){
+            
+            if(s[i]!=s[i-1]){
+                before = cur;
+                cur=1;
+            }
+            else{
+                cur++;
+            }
+            
+            if(before>=cur) count++;
+        }
+        
+        return count;
+    }
+};
