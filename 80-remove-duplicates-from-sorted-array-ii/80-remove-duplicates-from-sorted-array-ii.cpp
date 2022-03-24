ff09a4a9c7443578d7b73ddd949c3198aa68bcb5
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-03-24 18:18:52 +0900

    Time: 3 ms (93.80%), Space: 10.9 MB (75.43%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int sameCount=0;
-        int curNum=nums[0];
-        int jump=0;
-        int k=0;
-        for(int i=0; i+jump<nums.size(); i++){
+        int i=1, j=1;
+        int len=1;
+
+        for( ; j<nums.size(); i++,j++){
             
-            if(curNum!=nums[i+jump]){ 
-                curNum=nums[i+jump];
-                sameCount=1;
-            }
+            if(nums[i-1]==nums[j])
+                len++;
             else
-                sameCount++;
+                len=1;
             
-            if(sameCount<=2){
-                if(i+jump>=nums.size())
-                    break;
-                swap(nums[i],nums[i+jump]);
-                k++;
+            if(len<=2){
+                swap(nums[i],nums[j]);
                 continue;
             }
             
-            while(i+jump<nums.size()&&nums[i+jump]==curNum){
-                jump++;
-            }
+            while(j<nums.size()&&nums[i-1]==nums[j])
+                j++;
             
-            if(i+jump>=nums.size()) break;
-            
-            curNum = nums[i+jump];
-            sameCount=1;
-            swap(nums[i],nums[i+jump]);
-            k++;
+            len=1;
+            if(j<nums.size())swap(nums[i],nums[j]);
+            else break;
         }
         
-        return k;
+        return i;
     }
 };
