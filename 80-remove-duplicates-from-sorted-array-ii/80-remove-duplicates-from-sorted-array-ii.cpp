0dc9d903f427063e172e40eb9fa235cb5f8fb1fd
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-03-24 18:28:46 +0900

    Time: 12 ms (26.40%), Space: 10.9 MB (38.89%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int  j=1;
-        int len=1;
-
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i-1]==nums[i])
-                len++;
-            else
-                len=1;
+        int sameCount=0;
+        int curNum=nums[0];
+        int jump=0;
+        int k=0;
+        for(int i=0; i+jump<nums.size(); i++){
             
-            if(len<=2)
-                nums[j++] = nums[i];
+            if(curNum!=nums[i+jump]){ 
+                curNum=nums[i+jump];
+                sameCount=1;
+            }
+            else
+                sameCount++;
+            
+            if(sameCount<=2){
+                if(i+jump>=nums.size())
+                    break;
+                swap(nums[i],nums[i+jump]);
+                k++;
+                continue;
+            }
+            
+            while(i+jump<nums.size()&&nums[i+jump]==curNum){
+                jump++;
+            }
+            
+            if(i+jump>=nums.size()) break;
+            
+            curNum = nums[i+jump];
+            sameCount=1;
+            swap(nums[i],nums[i+jump]);
+            k++;
         }
         
-        return j;
+        return k;
     }
 };
