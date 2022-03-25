cbf4ca852a2924b0ed7047f200ad6e6a5fdec59f
NONA <65950955+SeonuJeong@users.noreply.github.com>
2022-03-25 22:41:17 +0900

    Time: 8 ms (60.74%), Space: 14.2 MB (31.66%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,8 +1,40 @@
 class Solution {
 public:
+    
+    vector<int> nums;
+    int target;
     bool search(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target) return true;
+        this->nums = nums;
+        this->target = target;
+        
+        return binary(0,nums.size()-1);
+    }
+    
+    bool binary(int le,int ri){
+        
+        while(le<=ri){
+            
+            int mid=(le+ri)/2;
+            
+            if(nums[mid]==target) return true;
+            
+            if(nums[mid]==nums[le]){
+                le++;
+                continue;
+            }
+            
+            if(nums[mid]<target){
+                if(nums[le]>nums[mid] && nums[le]<=target)
+                    ri = mid-1;
+                else
+                    le = mid+1;
+            }
+            else if(nums[mid]>target){
+                if(nums[ri]<nums[mid] && nums[ri]>=target)
+                    le = mid+1;
+                else
+                    ri = mid-1;
+            }
         }
         
         return false;
