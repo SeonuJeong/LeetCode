23ebd6fff451807149cee223c63b2c209149707b
SeonuJeong <65950955+SeonuJeong@users.noreply.github.com>
2022-04-04 20:45:40 +0900

    Time: 3 ms (56.05%), Space: 6.8 MB (14.51%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    vector<int> cache;
+    vector<int> nums;
+    int combinationSum4(vector<int>& nums, int target) {
+        this->nums = nums;
+        cache.assign(target+1,-1);
+        
+        return combination(target);
+    }
+    
+    int combination(int curTotal){
+        
+        if(curTotal==0){
+            return 1;
+        }
+        
+        if(curTotal<0)
+            return 0;
+        
+        if(cache[curTotal]!=-1)
+            return cache[curTotal];
+        
+        int result=0;
+        for(int i=0; i<nums.size(); i++)
+            result += combination(curTotal-nums[i]);
+        
+        cache[curTotal] = result;
+        return result;
+    }
+};
