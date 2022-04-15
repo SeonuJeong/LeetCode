a073db231741c15cbb2487f033851131dcb99fc2
SeonuJeong <65950955+SeonuJeong@users.noreply.github.com>
2022-04-15 23:06:22 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (85.94%) - LeetHub

--- 161-one-edit-distance/161-one-edit-distance.cpp
+++ 161-one-edit-distance/161-one-edit-distance.cpp
@@ -24,7 +24,6 @@ public:
         
         int count=0;
         for(int i=0; i<longest.length(); i++){
-            
             if(situ!=2&&longest[i]!=shortest[i-count]){
                 count++;
             }
