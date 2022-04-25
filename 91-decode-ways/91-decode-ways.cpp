f2965c22a0b24ffd074ba9d3989ce0d7b7123f80
SeonuJeong <65950955+SeonuJeong@users.noreply.github.com>
2022-04-25 22:57:02 +0900

    Time: 3 ms (53.58%), Space: 6.3 MB (25.69%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,48 +1,43 @@
 class Solution {
-public:
+public://1030
+    //dp
+    
+    vector<int> cache;
     string s;
     int numDecodings(string s) {
-        this->s =s;
-        
-        if(s[0]=='0') return 0;
-        if(s.length()==1) return 1;
-        
-        vector<int> dp(s.length(),0);
-        dp[0]=1;
-        
-        for(int i=1; i<s.length(); i++){
-            
-            if(s[i]!='0'){
-                dp[i] += dp[i-1];
-            }
-            
-            bool flag = checkTwo(i);
-            if(flag){
-                if(i!=1)
-                    dp[i] += dp[i-2];
-                else
-                    dp[i] += 1;
-            }
-            else if(!flag && s[i]=='0'){
-                return 0;
-            }
-        }
-        
-        
-        return dp[s.length()-1];
-        
+        cache.assign(s.length(),-1);
+        this->s = s;
+        return dp(0);
     }
     
-    bool checkTwo(int cur){
+    int dp(int pos){
         
-        if(s[cur-1]=='0') return false;
+        if(pos==s.length())
+            return 1;
         
-        int tmp = stoi(s.substr(cur-1,2));
+        if(s[pos]=='0')
+            return 0;
         
-        if(tmp>=10 && tmp<=26){
-            return true;
+        if(cache[pos]!=-1)
+            return cache[pos];
+        
+        int result=0;
+        
+        result += dp(pos+1);
+        
+        if(pos+1==s.length()){
+            cache[pos]=result;
+            return result;
         }
+        string tmp = s.substr(pos,2);
+        int convert = stoi(tmp);
         
-        return false;
+        if(convert>=1 && convert<=26)
+            result += dp(pos+2);
+        
+        
+        cache[pos] = result;
+        return result;
     }
+    
 };
