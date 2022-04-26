f6020a28911f971ddd7920d34e3fec08baf50c4b
SeonuJeong <65950955+SeonuJeong@users.noreply.github.com>
2022-04-26 19:58:20 +0900

    Time: 71 ms (92.33%), Space: 23.4 MB (33.56%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,28 +1,57 @@
 class Solution {
 public:
+    //0710
+    
+    vector<int> nums;
+    vector<vector<int>> result;
+    
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        vector<vector<int>> res;
-        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i)
-            if (i == 0 || nums[i - 1] != nums[i]) {
-                twoSumII(nums, i, res);
-            }
-        return res;
+        if(nums.size()<3)
+            return result;
+        
+        sort(nums.begin(),nums.end());
+        this->nums = nums;
+        
+        
+        for(int i=0; i<nums.size()-2 ; i++){
+            if(i!=0 && nums[i]==nums[i-1])
+                continue;
+            
+            twoSum(i,-1*nums[i]);
+        }
+        
+        return result;
     }
     
-    void twoSumII(vector<int>& nums, int i, vector<vector<int>> &res) {
-        int lo = i + 1, hi = nums.size() - 1;
-        while (lo < hi) {
-            int sum = nums[i] + nums[lo] + nums[hi];
-            if (sum < 0) {
-                ++lo;
-            } else if (sum > 0) {
-                --hi;
-            } else {
-                res.push_back({ nums[i], nums[lo++], nums[hi--] });
-                while (lo < hi && nums[lo] == nums[lo - 1])
-                    ++lo;
+    
+    void twoSum(int pos, int target){
+        
+        int le=pos+1;
+        int ri=nums.size()-1;
+        
+        
+        while(le<ri){
+            int sum= nums[le]+nums[ri];
+            if(sum==target){
+                vector<int> output;
+                output={nums[pos],nums[le],nums[ri]};
+                result.push_back(output);
+                
+                le++;
+                while(le<ri){
+                    if(nums[le]!=nums[le-1])
+                        break;
+                    else
+                        le++;
+                }
+            }
+            else if(sum<target){
+                le++;
+            }
+            else{
+                ri--;
             }
         }
     }
+    
 };
